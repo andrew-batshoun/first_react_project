4361f3b68cb3dbbaab923a6b3dea09b630ee6396
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-06-05 10:39:23 -0400

    created expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses.js";
+import NewExpense from "./components/NewExpense/NewExpense";
 import './App.css';
 
 
@@ -28,7 +29,7 @@ function App() {
 
     return (
         <div>
-            <h2>Lets get started</h2>
+            <NewExpense/>
             <Expenses items={expenses} />
         </div>
     );
